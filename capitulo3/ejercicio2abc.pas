206ec0e350c23f11c541ac1204245c341621c98b
chapuwu <lolochapa10@hotmail.com>
2023-06-05 19:45:12 -0300

    ejercicio2 del capitulo 3 terminado

--- capitulo3/ejercicio2abc.pas
+++ capitulo3/ejercicio2abc.pas
@@ -3,17 +3,21 @@ type
     fechasCasamientos = record
         numeroDia: integer;
         numeroMes: integer;
+        numeroAnio: integer;
     end;
 procedure leerFecha (var fecha: fechasCasamientos);
+var
+    casamientosEneroFebreroMarzo: integer;
+    casamientosPrimerosDiezDias: integer;
 begin
     while true do begin
         write('ingrese el numero del dia (1 - 31): ');
         read(fecha.numeroDia);
 
         if ((fecha.numeroDia < 1) or (fecha.numeroDia > 31)) then begin
-            writeln('se ingreso un dia incorrecto')
+            writeln('se ingreso un dia incorrecto');
             Break;
-        end
+        end;
 
         write('ingrese el numero del mes (1 - 12): ');
         read(fecha.numeroMes);
@@ -21,13 +25,35 @@ begin
         if ((fecha.numeroMes < 1) or (fecha.numeroMes > 12)) then begin
             writeln('se ingreso un mes incorrecto');
             Break;
-        end
+        end;
+
+        write('ingrese el anio (solo 2019): ');
+        read(fecha.numeroAnio);
+
+        writeln('el dia del casamiento fue: ', fecha.numeroDia, '-', fecha.numeroMes, '-', fecha.numeroAnio);
+
+        writeln('----------------------------');
+
+        if ((fecha.numeroAnio < 2019) or (fecha.numeroAnio > 2019)) then begin
+            writeln('error, solo existen casamientos del anio 2019');
+            Break;
+        end;
+
+
+        if ((fecha.numeroMes = 1) or (fecha.numeroMes = 2) or (fecha.numeroMes = 3)) then begin
+            casamientosEneroFebreroMarzo:= casamientosEneroFebreroMarzo + 1;
+        end;
+
+        if (fecha.numeroDia <= 10) then begin
+            casamientosPrimerosDiezDias:= casamientosPrimerosDiezDias + 1;
+        end;
 
     end;
+    writeln ('la cantidad de casamientos que hubo en enero, febrero y marzo es: ', casamientosEneroFebreroMarzo);
+    writeln ('la cantidad de casamientos de los primeros diez dias de cada mes es: ', casamientosPrimerosDiezDias);
 end;
 var
     fechaInformar: fechasCasamientos;
 begin
     leerFecha(fechaInformar);
-    writeln('el dia del casamiento fue: ', fechaInformar.numeroDia, '-', fechaInformar.numeroMes, '-2019');
 end.
