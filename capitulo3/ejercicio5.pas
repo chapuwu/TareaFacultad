5c71027181dc6dfc1400de0f3329326bf2ee58e7
chapuwu <lolochapa10@hotmail.com>
2023-06-15 20:47:07 -0300

    subiendo ejercicio 5 porque no tengo internet

--- capitulo3/ejercicio5.pas
+++ capitulo3/ejercicio5.pas
@@ -1,9 +1,58 @@
 program ejercicio5;
 type
-    auto = record;
+    auto = record
         marca: string;
         modelo: string;
         precio: integer;
     end;
+    autoLista = array of auto;
+procedure concesionaria;
+var
+    marcaNueva: auto;
+    marcaActual: auto;
+    autos: autoLista;
+    i: integer;
+    sumaNumeros: integer;
+    promedioTotal: real;
+begin
+    i:= 1;
+    sumaNumeros:= 0;
+    while true do begin
+        SetLength(autos, i);
+        write('ingrese la marca del auto (ingresar "ZZZ" para terminar lectura): ');
+        readln(autos[i].marca);
+        marcaActual:= autos[i];
+    
+        if (marcaActual.marca <> 'ZZZ') then begin
+            writeln('se termino la lectura');
+            Break;
+        end
 
-    autoSet = set of auto
+        write('ingresar el modelo del auto: ');
+        readln(autos[i].modelo);
+        write('ingresar el precio del auto: ');
+        readln(autos[i].precio);
+
+        // ordenar elementos por Marcas
+        if (marcaActual.marca <> marcaNueva.marca) then begin
+            marcaNueva:= autos[i];
+        end
+
+        // verificar que la marca no haya cambiado
+        if (marcaActual.marca = marcaNueva.marca) then begin 
+            sumaNumeros:= sumaNumeros + autos[i]
+        end
+
+        // verificar que la marca cambio
+        if (autos[i].marca <> marcaNueva.marca) then begin
+            promedioTotal:= sumaNumeros / length(autos);
+        end
+
+        // maniana a hacelro vago hdp falta verificar que la marca cambio y sacar el promedio
+
+        i:= i + 1;
+    end;
+end;
+begin
+    concesionaria;
+end.
